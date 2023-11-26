2df5357a161e5a50c989115c4e2cda38e0407c67
Felipe Bortolini <felipebortolini2001@gmail.com>
2023-11-25 21:17:09 -0300

    esp32

--- esp_32_dht11.ino
+++ esp_32_dht11.ino
@@ -1,69 +1,64 @@
 #include <DHT.h>  // Including library for dht
- 
-#include <ESP8266WiFi.h>
- 
-String apiKey = "HYB4O6K7QARUS0JK";     //  Enter your Write API key from ThingSpeak
- 
-const char *ssid =  "VIVOFIBRA-8D50";     // replace with your wifi ssid and wpa2 key
-const char *pass =  "184E53AF32";
-// const char* server = "api.thingspeak.com";
-const char* server = "184.106.153.149";
 
-#define DHTPIN 4          //pin where the dht11 is connected
+#include <WiFi.h>
+
+String apiKey = "HYB4O6K7QARUS0JK";  //  Enter your Write API key from ThingSpeak
+
+const char *ssid = "VIVOFIBRA-8D50";  // replace with your wifi ssid and wpa2 key
+const char *pass = "184E53AF32";
+// const char* server = "api.thingspeak.com";
+const char *server = "184.106.153.149";
+
+#define DHTPIN 4  //pin where the dht11 is connected
 
 DHT dht(DHTPIN, DHT11);
 
 WiFiClient client;
- 
-void setup() 
-{
-       Serial.begin(115200);
-       delay(10);
-       dht.begin();
- 
-       Serial.println("Connecting to ");
-       Serial.println(ssid);
- 
-       WiFi.begin(ssid, pass);
- 
-      while (WiFi.status() != WL_CONNECTED) 
-     {
-            delay(500);
-            Serial.print(".");
-     }
-      Serial.println("");
-      Serial.println("WiFi connected");
- 
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  dht.begin();
+
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
 }
- 
-void loop() 
-{
+
+void loop() {
   // Serial.println("teste");
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   //float h = 2;
   //float t = 3;
-  
-  if (isnan(h) || isnan(t)) 
-  {
+
+  if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
 
-  if (client.connect(server,80))   //   "184.106.153.149" or api.thingspeak.com
-  {  
-                
+  if (client.connect(server, 80))  //   "184.106.153.149" or api.thingspeak.com
+  {
+
     String postStr = apiKey;
-    postStr +="&field1=";
+    postStr += "&field1=";
     postStr += String(t);
-    postStr +="&field2=";
+    postStr += "&field2=";
     postStr += String(h);
     postStr += "\r\n\r\n";
 
     client.print("POST /update HTTP/1.1\n");
     client.print("Host: api.thingspeak.com\n");
     client.print("Connection: close\n");
-    client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+    client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
     client.print("Content-Type: application/x-www-form-urlencoded\n");
     client.print("Content-Length: ");
     client.print(postStr.length());
@@ -77,9 +72,9 @@ void loop()
     Serial.println("%. Send to Thingspeak.");
   }
   client.stop();
- 
+
   Serial.println("Waiting...");
-  
+
   // thingspeak needs minimum 15 sec delay between updates
   delay(1000);
 }
