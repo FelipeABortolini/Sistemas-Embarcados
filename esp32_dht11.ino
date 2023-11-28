974b76e5fe2af13f18a35625e3b3a02bbb2f85e3
felipe <felipebortolini2001@gmail.com>
2023-11-28 19:28:29 -0300

    leitura e envio para thingspeak com tarefas

--- esp32_dht11.ino
+++ esp32_dht11.ino
@@ -1,80 +1,110 @@
-#include <DHT.h>  // Including library for dht
-
+#include <DHT.h>
 #include <WiFi.h>
+#include "ThingSpeak.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/timers.h>
 
-String apiKey = "QSR26T80SOHFOI13";  //  Enter your Write API key from ThingSpeak
+#define CHANNEL_ID 2358437
+#define CHANNEL_API_KEY "QSR26T80SOHFOI13"
 
-const char *ssid = "VIVOFIBRA-8D50";  // replace with your wifi ssid and wpa2 key
+#define DHTPIN 4
+
+const char *ssid = "VIVOFIBRA-8D50";
 const char *pass = "184E53AF32";
-const char* server = "api.thingspeak.com";
-// const char *server = "184.106.153.149";
-
-#define DHTPIN 4  //pin where the dht11 is connected
 
 DHT dht(DHTPIN, DHT11);
 
 WiFiClient client;
 
-void setup() {
-  Serial.begin(115200);
-  delay(10);
-  dht.begin();
+// Timer para leitura periódica do sensor
+TimerHandle_t timerDHTRead;
 
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
+/* Descritor de task */
+TaskHandle_t taskSend;
 
+// Função para ler dados do sensor DHT
+void readDHTSensor(TimerHandle_t xTimer) {
+  // Notifica a tarefa para iniciar a leitura do sensor
+  xTaskNotifyGive(taskSend);
+}
+
+// Função para enviar dados para o ThingSpeak
+void sendDataToThingSpeak(float temperature, float humidity) {
+  ThingSpeak.writeField(CHANNEL_ID, 1, temperature, CHANNEL_API_KEY);
+  ThingSpeak.writeField(CHANNEL_ID, 2, humidity, CHANNEL_API_KEY);
+  ThingSpeak.writeField(CHANNEL_ID, 3, 1, CHANNEL_API_KEY);
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.print(" degrees Celsius, Humidity: ");
+  Serial.print(humidity);
+  Serial.println("%. Sent to ThingSpeak.");
+
+  Serial.println("Waiting...");
+}
+
+// Função da tarefa de envio para o ThingSpeak
+void taskSendToThingSpeak(void *pvParameters) {
+  float temperature, humidity;
+
+  while (1) {
+    // Aguarda notificação para iniciar a leitura do sensor
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+    // Realiza a leitura do sensor DHT
+    temperature = dht.readTemperature();
+    humidity = dht.readHumidity();
+
+    // Envia os dados para o ThingSpeak
+    sendDataToThingSpeak(temperature, humidity);
+  }
+}
+
+// Função da tarefa de gerenciamento de WiFi
+void taskWiFi(void *pvParameters) {
   WiFi.begin(ssid, pass);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  while (1) {
+    // Verifica o status da conexão WiFi a cada 10 segundos
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
+
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("Conexão WiFi perdida. Reconectando...");
+
+      WiFi.begin(ssid, pass);
+
+      int attempts = 0;
+      while (WiFi.status() != WL_CONNECTED && attempts < 10) {
+        delay(1000);
+        attempts++;
+      }
+
+      if (WiFi.status() == WL_CONNECTED) {
+        Serial.println("Conectado à rede WiFi!");
+      } else {
+        Serial.println("Falha na reconexão WiFi. Verifique as credenciais.");
+      }
+    }
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  delay(10);
+  dht.begin();
+  ThingSpeak.begin(client);
+
+  // Inicializa o timer para leitura periódica do sensor DHT
+  timerDHTRead = xTimerCreate("timerDHTRead", pdMS_TO_TICKS(15000), pdTRUE, (void *)0, readDHTSensor);
+  xTimerStart(timerDHTRead, 0);
+
+  // Inicia as tarefas
+  xTaskCreatePinnedToCore(taskWiFi, "WiFiTask", 4096, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(taskSendToThingSpeak, "ThingSpeakTask", 4096, NULL, 1, &taskSend, 1);
 }
 
 void loop() {
-  // Serial.println("teste");
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  //float h = 2;
-  //float t = 3;
-
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-
-  if (client.connect(server, 80))  //   "184.106.153.149" or api.thingspeak.com
-  {
-
-    String postStr = apiKey;
-    postStr += "&field1=";
-    postStr += String(t);
-    postStr += "&field2=";
-    postStr += String(h);
-    postStr += "\r\n\r\n";
-
-    client.print("POST /update HTTP/1.1\n");
-    client.print("Host: api.thingspeak.com\n");
-    client.print("Connection: close\n");
-    client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
-    client.print("Content-Type: application/x-www-form-urlencoded\n");
-    client.print("Content-Length: ");
-    client.print(postStr.length());
-    client.print("\n\n");
-    client.print(postStr);
-
-    Serial.print("Temperature: ");
-    Serial.print(t);
-    Serial.print(" degrees Celcius, Humidity: ");
-    Serial.print(h);
-    Serial.println("%. Send to Thingspeak.");
-  }
-  client.stop();
-
-  Serial.println("Waiting...");
-
-  // thingspeak needs minimum 15 sec delay between updates
-  delay(1000);
+  
 }
